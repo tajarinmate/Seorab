ca83f66b10beb5b65cd053f9d13a9f1cc1645441
Yesong Choi <to06109@naver.com>
2023-08-22 01:40:10 +0900

    settings: proxy 설정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  // proxy 설정
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       destination: 'http://localhost:8080/:path*',
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
